03001a88f917d789ba3f4e4c058ea3c5c7516505
demonpie0369 <120589147+demonpie0369@users.noreply.github.com>
2025-03-21 09:00:28 +1300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ uniform sampler2D u_image;
 varying vec2 v_texCoord;
 
 void main() {
-    gl_FragColor = texture2D(u_image, v_texCoord);
+    gl_FragColor = /*texture2D(u_image, v_texCoord)*/ vec4(1.0, 0.0, 0.0, 1.0);
 }`;
 
 function createshader(gl, type, source) {
